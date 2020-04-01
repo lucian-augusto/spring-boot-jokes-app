a88a49913f634e491d1e59e23072c90566782954
Lucian Augusto <lucian.augusto.92@gmail.com>
2020-04-01 16:03:58 -0300

    Refactor the app to use xml configuration instead of Java Configuration (used on the previous commit).

--- src/main/java/com/lucianaugusto/jokesapp/config/ChuckConfiguration.java
+++ src/main/java/com/lucianaugusto/jokesapp/config/ChuckConfiguration.java
@@ -5,10 +5,10 @@ import org.springframework.context.annotation.Configuration;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 
-@Configuration
+//@Configuration
 public class ChuckConfiguration {
 
-	@Bean // Telling the Spring Framework that we want it as a bean
+//	@Bean // Telling the Spring Framework that we want it as a bean
 	// Adding the property from the JokeServiceIpml
 	public ChuckNorrisQuotes chuckNorrisQuotes() { // Only returns a new instance of ChuckNorrisQuotes
 		return new ChuckNorrisQuotes();
